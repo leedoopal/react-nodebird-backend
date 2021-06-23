12944c4ca6ae64e7294281fd47847623a6f639c4
leedoopal <39582981+leedoopal@users.noreply.github.com>
2021-06-23 23:00:23 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# react-nodebird-backend
+ nodebird의 backend를 만들어 보려고 한다
---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+  if (req.method === 'GET') {
+
+  } else if (req.method === 'POST') {
+
+  }
+  res.write('<h1>TEST</h1>');
+  res.write('hello node');
+  res.end('hello node');
+});
+server.listen(3065, () => {
+  console.log('start!');
+});
