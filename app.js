4d44408a00396f2f351c49ceff8def27ddb1eaa4
leedoopal <39582981+leedoopal@users.noreply.github.com>
2021-06-24 23:15:08 +0900

    prettier 추가

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-const http = require("http");
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
-  if (req.method === "GET") {
-  } else if (req.method === "POST") {
-  }
-  res.write("<h1>TEST</h1>");
-  res.write("hello node");
-  res.end("hello node");
-});
-server.listen(3065, () => {
-  console.log("start!");
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {});
+
+app.listen(3065, () => {
+  console.log('서버 실행 중');
 });
