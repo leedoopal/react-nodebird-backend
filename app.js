f87defe4f73f1da70e53517d195647ba14826ca0
leedoopal <39582981+leedoopal@users.noreply.github.com>
2021-06-24 23:21:53 +0900

    express로 라우팅하기

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {});
+app.get('/', (req, res) => {
+  res.send('hello express');
+});
+
+app.get('/api/posts', (req, res) => {
+  res.json([
+    {
+      id: 1,
+      content: 'hello',
+    },
+    {
+      id: 2,
+      content: 'hi',
+    },
+  ]);
+});
+
+app.post('/api/post', (req, res) => {
+  res.json('complete');
+});
 
 app.listen(3065, () => {
   console.log('서버 실행 중');
