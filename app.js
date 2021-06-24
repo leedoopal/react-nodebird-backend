588880f92c827008e9ce1f3f0ee7caca7cf2a43d
leedoopal <39582981+leedoopal@users.noreply.github.com>
2021-06-24 23:09:31 +0900

    eslint 추가

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
-  if (req.method === 'GET') {
-
-  } else if (req.method === 'POST') {
-
+  if (req.method === "GET") {
+  } else if (req.method === "POST") {
   }
-  res.write('<h1>TEST</h1>');
-  res.write('hello node');
-  res.end('hello node');
+  res.write("<h1>TEST</h1>");
+  res.write("hello node");
+  res.end("hello node");
 });
 server.listen(3065, () => {
-  console.log('start!');
+  console.log("start!");
 });
