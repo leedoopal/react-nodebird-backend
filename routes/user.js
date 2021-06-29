141f05bb62186d5e447d096bd04d969cbf5bd054
leedoopal <39582981+leedoopal@users.noreply.github.com>
2021-06-29 23:11:34 +0900

    회원가입 구현

---
+++ routes/user.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const { User } = require('../models');
+
+const router = express.Router();
+
+router.post('/', async (req, res, next) => {
+  try {
+    // 기존 user인지 check
+    const exUser = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (exUser) {
+      return res.status(403).send('이미 사용중인 아이디입니다.');
+    }
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 12);
+    await User.create({
+      email: req.body.email,
+      nickname: req.body.nickname,
+      password: hashedPassword,
+    });
+    res.status(200).send('ok');
+  } catch (error) {
+    console.error(error);
+    next(error); // status 500
+  }
+});
+
+module.exports = router;
