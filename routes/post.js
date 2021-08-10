c4b922bf0e15f6d0162b4d617acad94b78e86762
leedoopal <39582981+leedoopal@users.noreply.github.com>
2021-08-10 21:28:16 +0900

    게시글, 댓글 추가

--- routes/post.js
+++ routes/post.js
@@ -1,12 +1,47 @@
 const express = require('express');
+const { Post } = require('../models');
+const { isSignedIn } = require('./middlewares');
+
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  res.json('complete');
+router.post('/', isSignedIn, async (req, res, next) => {
+  try {
+    const post = await Post.create({
+      content: req.body.data,
+      UserId: req.user.id,
+    });
+    res.status(201).json(post);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
 });
 
-router.delete('/', (req, res) => {
+router.delete('/', isSignedIn, (req, res) => {
   res.json('delete');
+});
+
+router.post('/:postId/comment', isSignedIn, async (req, res, next) => {
+  try {
+    const post = await Post.findOne({
+      where: { id: req.params.postId },
+    });
+
+    // post가 존재하지 않다면
+    if (!post) {
+      return res.status(403).send('존재하지 않는 게시글 입니다.');
+    }
+
+    const comment = await Comment.create({
+      content: req.body.data,
+      PostId: req.params.postId,
+      UserId: req.user.id,
+    });
+    res.status(201).json(comment);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
 });
 
 module.exports = router;
