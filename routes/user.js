10ff50cf931aec38a7028ee5a800e10339ccf030
leedoopal <39582981+leedoopal@users.noreply.github.com>
2021-08-17 17:28:23 +0900

    닉네임 업데이트 추가

--- routes/user.js
+++ routes/user.js
@@ -123,4 +123,22 @@ router.put('/logout', isSignedIn, (req, res, next) => {
   res.send('ok');
 });
 
+router.patch('/nickname', isSignedIn, async (req, res, next) => {
+  try {
+    await User.update(
+      {
+        nickname: req.body.nickname,
+      },
+      {
+        where: { id: req.user.id },
+      },
+    );
+
+    res.status(200).json({ nickname: req.body.nickname });
+  } catch (err) {
+    console.err(err);
+    next(err);
+  }
+});
+
 module.exports = router;
