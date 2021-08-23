e00e794d7b115c5008bdd7322e1f6dee4d40c2f0
leedoopal <39582981+leedoopal@users.noreply.github.com>
2021-08-23 23:34:41 +0900

    express.static 미들웨어 추가
    이미지 업로드를 위한 미들웨어

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const express = require('express');
+const path = require('path');
 const cors = require('cors');
+const express = require('express');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const passport = require('passport');
@@ -32,6 +33,7 @@ app.use(
     credentials: true,
   }),
 );
+app.use('/', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
